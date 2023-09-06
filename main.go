84365fb0019bb7f1b53265853bcaa548a9cceb17
Nhat Le <nhat.n.le@sjsu.edu>
2023-09-05 23:56:54 -0700

    learn data types

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+	//fmt.Println("Hello World!")
+	// var strOne string = "string"
+	// var strTwo = "string2"
+	// var strThree string
+
+	// // short assignment, type inference
+	// numberOne := 23
+
+	// fmt.Println(strOne, numberOne, strTwo, strThree)
+
+	// // same line assignment
+	// var v1, v2 = true, 123
+
+	// fmt.Println(v1, v2)
+
+	// // type casting
+	// v1 := 34.53
+	// var v2 int
+	// v2 = int(v1)
+
+	// fmt.Println(v2)
+
+	// const v1 = "constant" // can not use short assignment
+
+	// var v1 = 10
+	// const v2 = 40  // constants must be computed at compile time, not run-time
+
+	//s1 := true
+	s2 := 2123
+
+	fmt.Printf("%b", s2)
+}
