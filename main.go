42493dc781c97a8eb6967effc6173bba4f348bbc
Nhat Le <nhat.n.le@sjsu.edu>
2023-09-15 00:18:15 -0700

    learn standard lib

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	//fmt.Println("Hello World!")
@@ -31,7 +34,37 @@ func main() {
 	// const v2 = 40  // constants must be computed at compile time, not run-time
 
 	//s1 := true
-	s2 := 2123
+	// s2 := 2123
 
-	fmt.Printf("%b", s2)
+	// fmt.Printf("%b", s2)
+
+	// a, b := addAndSubtract(1, 3)
+
+	// fmt.Print(a, b)
+
+	// Array
+	// var arrInt [4]int = [4]int{1, 2, 3, 4}
+	// arrStr := [3]string{"1", "2", "3"}
+	// fmt.Print(arrInt, len(arrInt))
+
+	// // Slices
+	// var arrInt = []int{1, 2, 3, 4}
+	// fmt.Println(arrInt, len(arrInt))
+	// // Add items into a slice
+	// arrInt = append(arrInt, 5, 6)
+	// fmt.Println(arrInt, len(arrInt))
+	// // Slice Range
+	// arrIntRange := arrInt[0:3]
+	// fmt.Printf("%T", arrIntRange) // print Type of object, can be accomplished with reflect pkg
+
+	result := strings.ReplaceAll("Hello World Hello", "Hello", "World")
+
+	fmt.Println(result)
+
 }
+
+// func addAndSubtract(a, b int) (total, diff int) {
+// 	total = a + b
+// 	diff = a - b
+// 	return 0, 1
+// }
